2b910538cb562d0222effa1e6e5f3d5f18c60f3f
D3press3dd <anthonyrosman20@gmail.com>
2022-03-09 22:10:41 -0400

    adding dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Countries from "./components/Countries";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const [darkTheme, setDarkTheme] = useState(false);
+  const [darkTheme, setDarkTheme] = useState(true);
+
+  useEffect(() => {
+    darkTheme
+      ? document.body.classList.add("dark-theme")
+      : document.body.classList.remove("dark-theme");
+  }, [darkTheme]);
+
   return (
-    <main className={darkTheme ? "dark-theme " : ""}>
+    <main>
       <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
       <Countries />
     </main>
